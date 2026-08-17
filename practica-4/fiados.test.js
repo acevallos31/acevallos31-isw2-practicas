fbbec0a12a3bf454b4d8523b3801c43dabc58b1e
Axeell Cevallos <acevallos31@gmail.com>
2026-08-16 23:25:13 -0600

    test(fiados): agregar test de dias no numericos

--- practica-4/fiados.test.js
+++ practica-4/fiados.test.js
@@ -28,3 +28,7 @@ test("retorna 0 cuando no hay dias vencidos", () => {
 test("retorna 0 cuando el monto es negativo", () => {
     assertEqual(calcularMora(-1000, 5), 0);
 });
+
+test("retorna 0 cuando los dias no son numericos", () => {
+    assertEqual(calcularMora(1000, "abc"), 0);
+});
