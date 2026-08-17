82c4726b5825324bf93654cca5ad7c4732e9e7ea
Axeell Cevallos <acevallos31@gmail.com>
2026-08-16 23:26:43 -0600

    refactor(fiados): agrupar validaciones de entrada

--- practica-4/fiados.js
+++ practica-4/fiados.js
@@ -1,11 +1,11 @@
 function calcularMora(monto, diasVencidos) {
     const porcentajeMora = 0.05;
 
-    if (typeof diasVencidos !== "number") {
-        return 0;
-    }
-
-    if (monto < 0 || diasVencidos <= 0) {
+    if (
+        typeof diasVencidos !== "number" ||
+        monto < 0 ||
+        diasVencidos <= 0
+    ) {
         return 0;
     }
 
