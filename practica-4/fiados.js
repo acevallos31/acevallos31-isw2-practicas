6eac9aa918bd778089cfaf022e3c4d2149938000
Axeell Cevallos <acevallos31@gmail.com>
2026-08-16 23:26:00 -0600

    feat(fiados): validar dias no numericos

--- practica-4/fiados.js
+++ practica-4/fiados.js
@@ -1,6 +1,10 @@
 function calcularMora(monto, diasVencidos) {
     const porcentajeMora = 0.05;
 
+    if (typeof diasVencidos !== "number") {
+        return 0;
+    }
+
     if (monto < 0 || diasVencidos <= 0) {
         return 0;
     }
